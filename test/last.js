283bdb9b5dfbf61701494f212bc203e2f25d1cb4
Michael Mathews <micmath@gmail.com>
2010-08-06 00:19:54 +0100

    Fixed bug with $last that caused it to fail on nested reduce loops.

--- test/last.js
+++ test/last.js
@@ -6,10 +6,43 @@ var template = require("../lib/normal-template"),
 exports.testReduce$last = function(test){
 	test.expect(1);
 	
-	var data = {items: ["milk", "bread", "pickles"]},
-		t	 = compile("{:r items}{=.}{:if $last}.{:else}, {/:if}{/:r}");
+	var data = {list: ["milk", "bread", "pickles"]},
+		t	 = compile("{:r list}{=.}{:if $last}.{:else}, {/:if}{/:r}");
 	
 	test.same("milk, bread, pickles.", t(data));
+	
+	test.done();
+};
+
+exports.testReduce$lastOutside = function(test){
+	test.expect(1);
+	
+	var data = { list: ['bananas'] },
+		t	 = compile("{:if $last}yes{:else}no{/:if} {:r list}{=.}{/:r} {:if $last}{:else}today{/:if}");
+	
+	test.same("no bananas today", t(data));
+	
+	test.done();
+};
+
+exports.testReduce$lastSingle = function(test){
+	test.expect(1);
+	
+	var data = { list: ['chocolate'] },
+		t	 = compile("{:r list}{=.}{:if $last}!{:else}, {/:if}{/:r}");
+	
+	test.same("chocolate!", t(data));
+	
+	test.done();
+};
+
+exports.testReduce$lastNested = function(test){
+	test.expect(1);
+	
+	var data = { list: [{name:"dairy", items:["milk", "butter"]}, {name:"veg", items:["lettuce", "peppers", "pickles"]}] },
+		t	 = compile("{:r list}{=name}: {:r items}{=.}{:if $last}.{:else}, {/:if}{/:r}{:if $last}{:else} & {/:if}{/:r}");
+	
+	test.same("dairy: milk, butter. & veg: lettuce, peppers, pickles.", t(data));
 	
 	test.done();
 };
