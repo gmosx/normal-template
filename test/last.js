61b84e67b285d0c4f493c1bcbdd1067b00006da4
Michael Mathews <micmath@gmail.com>
2010-08-06 00:36:57 +0100

    Checked in Rhino and Node. Minor tweaks to implementation and tests.

--- test/last.js
+++ test/last.js
@@ -4,6 +4,7 @@ var template = require("../lib/normal-template"),
 	compile = template.compile;
 
 exports.testReduce$last = function(test){
+	// when reducing, $last should only be true on the last loop
 	test.expect(1);
 	
 	var data = {list: ["milk", "bread", "pickles"]},
@@ -15,6 +16,7 @@ exports.testReduce$last = function(test){
 };
 
 exports.testReduce$lastOutside = function(test){
+	// $last should be undefined outside of a reduce loop
 	test.expect(1);
 	
 	var data = { list: ['bananas'] },
@@ -26,6 +28,7 @@ exports.testReduce$lastOutside = function(test){
 };
 
 exports.testReduce$lastSingle = function(test){
+	// when reducing a list with a single item, $last should be true
 	test.expect(1);
 	
 	var data = { list: ['chocolate'] },
@@ -37,12 +40,13 @@ exports.testReduce$lastSingle = function(test){
 };
 
 exports.testReduce$lastNested = function(test){
+	// when a reduce loop is nested within another reduce loop, $last should stack
 	test.expect(1);
 	
 	var data = { list: [{name:"dairy", items:["milk", "butter"]}, {name:"veg", items:["lettuce", "peppers", "pickles"]}] },
-		t	 = compile("{:r list}{=name}: {:r items}{=.}{:if $last}.{:else}, {/:if}{/:r}{:if $last}{:else} & {/:if}{/:r}");
+		t	 = compile("{:r list}{=name}: {:r items}{=.}{:if $last}{:else}, {/:if}{/:r}{:if $last}{:else} & {/:if}{/:r}");
 	
-	test.same("dairy: milk, butter. & veg: lettuce, peppers, pickles.", t(data));
+	test.same("dairy: milk, butter & veg: lettuce, peppers, pickles", t(data));
 	
 	test.done();
 };
