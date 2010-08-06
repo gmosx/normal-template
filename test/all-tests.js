d90b7f22446d22025dc7fca869ef995be376401b
George Moschovitis <george.moschovitis@gmail.com>
2010-08-06 09:28:58 +0300

    Update last test for commonjs compatible testing.

--- test/all-tests.js
+++ test/all-tests.js
@@ -1,5 +1,6 @@
 exports.testNormalTemplate = require("./normal-template");
 exports.testNormalTPP = require("./normal-template/tpp");
+exports.testLast = require("./last");
 
 if (module === require.main) {
     require("test").run(exports);
--- test/last.js
+++ test/last.js
@@ -1,57 +1,40 @@
-// try this: node last.js
+var assert = require("assert");
 
 var template = require("../lib/normal-template"),
 	compile = template.compile;
 
-exports.testReduce$last = function(test){
+exports.testReduce$last = function (test) {
 	// when reducing, $last should only be true on the last loop
-	test.expect(1);
-	
 	var data = {list: ["milk", "bread", "pickles"]},
 		t	 = compile("{:r list}{=.}{:if $last}.{:else}, {/:if}{/:r}");
 	
-	test.same("milk, bread, pickles.", t(data));
-	
-	test.done();
+	assert.equal("milk, bread, pickles.", t(data));
 };
 
-exports.testReduce$lastOutside = function(test){
+exports.testReduce$lastOutside = function (test) {
 	// $last should be undefined outside of a reduce loop
-	test.expect(1);
-	
 	var data = { list: ['bananas'] },
 		t	 = compile("{:if $last}yes{:else}no{/:if} {:r list}{=.}{/:r} {:if $last}{:else}today{/:if}");
 	
-	test.same("no bananas today", t(data));
-	
-	test.done();
+	assert.equal("no bananas today", t(data));
 };
 
-exports.testReduce$lastSingle = function(test){
+exports.testReduce$lastSingle = function (test) {
 	// when reducing a list with a single item, $last should be true
-	test.expect(1);
-	
 	var data = { list: ['chocolate'] },
 		t	 = compile("{:r list}{=.}{:if $last}!{:else}, {/:if}{/:r}");
 	
-	test.same("chocolate!", t(data));
-	
-	test.done();
+	assert.equal("chocolate!", t(data));
 };
 
-exports.testReduce$lastNested = function(test){
+exports.testReduce$lastNested = function (test) {
 	// when a reduce loop is nested within another reduce loop, $last should stack
-	test.expect(1);
-	
 	var data = { list: [{name:"dairy", items:["milk", "butter"]}, {name:"veg", items:["lettuce", "peppers", "pickles"]}] },
 		t	 = compile("{:r list}{=name}: {:r items}{=.}{:if $last}{:else}, {/:if}{/:r}{:if $last}{:else} & {/:if}{/:r}");
 	
-	test.same("dairy: milk, butter & veg: lettuce, peppers, pickles", t(data));
-	
-	test.done();
+	assert.equal("dairy: milk, butter & veg: lettuce, peppers, pickles", t(data));
 };
 
-if(module.id == '.'){
-	var testrunner = require('nodeunit').testrunner;
-	testrunner.run([__filename]);
+if (module === require.main) {
+    require("test").run(exports);
 }
